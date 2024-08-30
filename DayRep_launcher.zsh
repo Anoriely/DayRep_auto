81828d009bb49a3bda5c76193b1afa475e2b4bbc
Dmitrijs Ciruks <dmitrijsciruks@MacBook-Air-Dmitrijs.local>
2024-08-30 15:29:11 +0300

    launcher modified for homebrew

--- DayRep_launcher.zsh
+++ DayRep_launcher.zsh
@@ -2,17 +2,27 @@
 
 if ! command -v R &> /dev/null
 then
-    echo "R не установлен. Начинаю установку..."
+    echo "R не установлен."
 
-    curl -O https://cloud.r-project.org/bin/macosx/base/R-latest.pkg
+    if ! command -v brew &> /dev/null
+    then
+        echo "Homebrew не установлен. Начинаю установку..."
+        
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-    sudo installer -pkg R-latest.pkg -target /
+        echo "Homebrew успешно установлен."
+    else
+        echo "Homebrew уже установлен."
+    fi
+
+    echo "Начинаю установку R через Homebrew..."
+    export PATH=/opt/homebrew/bin:$PATH
+    brew install --cask R
 
     echo "R успешно установлен."
 else
     echo "R уже установлен."
 fi
 
-# Запуск Rscript
 SCRIPT_DIR="$(cd "$(dirname "${(%):-%N}")" && pwd)"
 Rscript "$SCRIPT_DIR/DayRep_auto.R"
